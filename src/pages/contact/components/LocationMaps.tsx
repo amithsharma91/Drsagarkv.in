1b30c259285898bb885f5b31ccf971726bacb5cc
amithsharma91 <amithsharma8383@gmail.com>
2026-06-07 21:49:52 +0530

    Update LocationMaps.tsx

--- src/pages/contact/components/LocationMaps.tsx
+++ src/pages/contact/components/LocationMaps.tsx
@@ -7,7 +7,7 @@ const LOCATIONS = [
     area: 'Basavanagudi',
     address: 'No. 45, South End Road, Basavanagudi, Bangalore 560004',
     mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1!2d77.5731!3d12.9516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDU3JzA1LjgiTiA3N8KwMzQnMjMuMiJF!5e0!3m2!1sen!2sin!4v1',
-    timing: 'Mon - Sat: 9 AM - 7 PM',
+    timing: 'Mon - Sat: 6 PM - 9 PM',
     phone: '+91 80508 16686',
     icon: 'ri-building-line',
   },
